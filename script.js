f77afaab0a3fe013a0fefaa8c0418e7741a75813
Deep1Shikha <47516502+Deep1Shikha@users.noreply.github.com>
2021-02-14 00:26:40 +0530

    v1.2
    Added the feature for toggling between celsius and fahrenheit

--- script.js
+++ script.js
@@ -80,3 +80,24 @@ function displayDefaultWeather(){
     descElement.innerHTML = "mist";
     locationElement.innerHTML = "Daltonganj, IN";
 }
+
+//C to F conversion
+function celsiusToFahrenheit(temperature){
+    return (temperature * 9/5) + 32;
+}
+
+// When the user clicks on the temperature element
+tempElement.addEventListener("click", function(){
+  if(weather.temperature.value === undefined) return;
+
+  if(weather.temperature.unit == "celsius"){
+     let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+     fahrenheit = Math.floor(fahrenheit);
+      tempElement.innerHTML = `${fahrenheit}*<span>F</span>`;
+      weather.temperature.unit = "Fahrenheit";
+  }else{
+      tempElement.innerHTML = `${weather.temperature.value}*<span>C</span>`;
+      weather.temperature.unit = "celsius";
+  }
+
+});
